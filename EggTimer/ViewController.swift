e164b4b0b730ca1bb597d73cd8b1868e44fbb274
ttt-net <tenten102matsuyama@gmail.com>
2022-06-02 03:42:27 +0900

    Add IBOutlet and IBAction

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+    }
 
 }
 
