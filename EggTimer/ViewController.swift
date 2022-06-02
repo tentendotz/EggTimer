0baf1a64bc365cb891276050d9106679896713fc
ttt-net <tenten102matsuyama@gmail.com>
2022-06-03 01:20:36 +0900

    Create progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,16 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
     
     var timer = Timer()
     var player: AVAudioPlayer!
     var secondsRemaining = 0
+    var increasePercentage: Float = 0.0
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
         let hardness = sender.currentTitle!
         secondsRemaining = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        increasePercentage = Float(1.0) / Float(secondsRemaining)
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(
             timeInterval: 1.0,
@@ -36,7 +41,7 @@ class ViewController: UIViewController {
     @objc func updateTimer(){
         if secondsRemaining > 0 {
             secondsRemaining -= 1
-            
+            progressBar.progress += increasePercentage
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
