fd90c9038d45348bf1259973a5ddc5adb0b18127
ttt-net <tenten102matsuyama@gmail.com>
2022-06-03 01:31:42 +0900

    Rename property and Upgrade timer logic

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,20 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
-    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
     var player: AVAudioPlayer!
-    var secondsRemaining = 0
-    var increasePercentage: Float = 0.0
-
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
         timer.invalidate()
         let hardness = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         
         progressBar.progress = 0.0
-        increasePercentage = Float(1.0) / Float(secondsRemaining)
+        secondsPassed = 0
         titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(
@@ -39,22 +40,23 @@ class ViewController: UIViewController {
     }
     
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            secondsRemaining -= 1
-            progressBar.progress += increasePercentage
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
             playSound()
         }
     }
-    
 
     func playSound() {
         let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
+    
 }
 
 
